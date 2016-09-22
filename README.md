a12eef7c321b28325e91d1ee49becc4bc566eeaa
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-21 19:58:33 -0500

    Third commit

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 
 #####I am not a sports fan, so I chose to create a model based on my friend's favorite team, Real Madrid.
 #####I do not know if the information is completely correct, but I hope the object will be used correctly.
+
+
+
+######The following is the console output of the assignment:
+
+'''
+Real Madrid is a very popular football/soccer team from Spain.  The team was founded in 1902 and is currently one of the most valuable teams, worth $3.65 billion.  Their main rival is Barcelona.  Some of the most famous players, past and present, are Cristiano Ronaldo, Raul, Zinedine Zidane, Iker Casillas, and Santillana.  Real Madrid has won many competitions, having won the Liga, the domestic competition in Spain, 32 times, and the international EUFA competition 11 times.
+'''
+
+######Please check the code to see how this output was created.
--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var team =function() {
-  this.teamName = teamName;
-  this.sport = sport;
-  this.country = country;
-  this.firstYear = firstYear;
-  this.players = players;
-  this.timesWon = timesWon;
-  this.
-};
+var madrid = {
+  teamName: 'Real Madrid',
+  sportEu: 'football',
+  sportUs: 'soccer',
+  country: 'Spain',
+  firstYear: 1902,
+  topPlayers: ['Cristiano Ronaldo', 'Raul', 'Zinedine Zidane', 'Iker Casillas', 'Santillana'],
+  value: '3.65 billion',
+  rival: 'Barcelona',
+  domestic: 'Liga',
+  international: 'EUFA',
+  timesWonLiga: 32,
+  timesWonEUFA: 11
+}
+var players = '';
+while (madrid.topPlayers.length > 0){
+  if (madrid.topPlayers.length > 1) {
+    players += (madrid.topPlayers.shift() + ', ');
+  } else {
+    players += ('and ' + madrid.topPlayers.shift());
+  }
+}
+
+console.log(madrid.teamName + ' is a very popular ' + madrid.sportEu + '/' + madrid.sportUs + ' team from ' +
+madrid.country + '.  The team was founded in ' + madrid.firstYear + ' and is currently one of the most valuable teams, worth $' +
+madrid.value + '.  Their main rival is ' + madrid.rival + '.  Some of the most famous players, past and present, are ' + players +
+'.  ' + madrid.teamName + ' has won many competitions, having won the ' + madrid.domestic + ', the domestic competition in ' +
+madrid.country + ', ' + madrid.timesWonLiga + ' times, and the international ' + madrid.international +
+' competition ' + madrid.timesWonEUFA + ' times.');
